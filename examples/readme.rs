6a1c7e8146c08b26c0b9bd3d61095cd787640e21
ActuallyHappening (Ubuntu) <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-28 18:57:48 +1000

    fix

--- examples/readme.rs
+++ examples/readme.rs
@@ -13,7 +13,7 @@
 //!     - `circle_label: String` is controllable as a textbox,
 //!     - `non_changing_global_value: i8` is a non-interactive field.
 
-use egui_controls::ControlPanel;
+use bevy_egui_controls::ControlPanel;
 
 /// Some config data for the
 #[derive(Debug, Clone, ControlPanel)]
@@ -33,48 +33,48 @@ pub struct CirclePackingAlgorithmConfig {
     pub non_changing_global_value: i8,
 }
 
-/// Some initial values for the config that make sense.
-impl Default for CirclePackingAlgorithmConfig {
-    fn default() -> Self {
-        Self {
-            radius: 12.0,
-            max_overlap_count: 10,
-            circle_label: "Some text".to_string(),
-            non_changing_global_value: 42,
-        }
-    }
-}
+// /// Some initial values for the config that make sense.
+// impl Default for CirclePackingAlgorithmConfig {
+//     fn default() -> Self {
+//         Self {
+//             radius: 12.0,
+//             max_overlap_count: 10,
+//             circle_label: "Some text".to_string(),
+//             non_changing_global_value: 42,
+//         }
+//     }
+// }
 
-use eframe::{egui, Frame};
+// use bevy_egui::{egui, Frame};
 
-#[derive(Debug, Clone, Default)]
-pub struct MyApp {
-    settings: CirclePackingAlgorithmConfig,
-}
+// #[derive(Debug, Clone, Default)]
+// pub struct MyApp {
+//     settings: CirclePackingAlgorithmConfig,
+// }
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
-        egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
-            // Embed the settings panel
-            // directly into your ui.
-            self.settings.ui(ui);
-            // Add this the struct's debug repr if you want
-            // to see the values getting updated as you tweak
-            // the settings via the ui.
-            ui.vertical(|ui| {
-                ui.code(format!("{:#?}", &self.settings));
-            });
-        });
-    }
-}
+// impl bevy_egui::App for MyApp {
+//     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
+//         egui::CentralPanel::default().show(ctx, |ui: &mut egui::Ui| {
+//             // Embed the settings panel
+//             // directly into your ui.
+//             self.settings.ui(ui);
+//             // Add this the struct's debug repr if you want
+//             // to see the values getting updated as you tweak
+//             // the settings via the ui.
+//             ui.vertical(|ui| {
+//                 ui.code(format!("{:#?}", &self.settings));
+//             });
+//         });
+//     }
+// }
 
-// Write the usual eframe entrypoint.
+// Write the usual bevy_egui entrypoint.
 pub fn main() {
-    let options = ::eframe::NativeOptions {
-        resizable: true,
-        initial_window_size: Some(::eframe::egui::vec2(2000.0, 500.0)),
-        ..Default::default()
-    };
-    let app = MyApp::default();
-    ::eframe::run_native("readme", options, Box::new(|_| Box::new(app))).unwrap();
+    // let options = ::bevy_egui::NativeOptions {
+    //     resizable: true,
+    //     initial_window_size: Some(::bevy_egui::egui::vec2(2000.0, 500.0)),
+    //     ..Default::default()
+    // };
+    // let app = MyApp::default();
+    // ::bevy_egui::run_native("readme", options, Box::new(|_| Box::new(app))).unwrap();
 }
--- src/parse.rs
+++ src/parse.rs
@@ -3,131 +3,131 @@ use proc_macro2::TokenStream as TokenStream2;
 
 use quote::{quote, ToTokens};
 use syn::{
-    Data, DataStruct, DeriveInput, Expr, ExprLit, Fields, Lit, Meta, MetaList, MetaNameValue,
+	Data, DataStruct, DeriveInput, Expr, ExprLit, Fields, Lit, Meta, MetaList, MetaNameValue,
 };
 
 /// Parse struct fields into an iterator over the
 /// doc comments of fields in the order of definition.
 fn parse_doc_comments_from_fields(fields: &Fields) -> impl Iterator<Item = String> + '_ {
-    fields.iter().map(|field| {
-        let mut doc_comments = vec![];
+	fields.iter().map(|field| {
+		let mut doc_comments = vec![];
 
-        // Every individual doc comment is an attr.
-        field.attrs.iter().for_each(|attr| {
-            if let Meta::NameValue(MetaNameValue { path, value, .. }) = &attr.meta {
-                path.segments.iter().for_each(|segment| {
-                    if segment.ident == "doc" {
-                        if let Expr::Lit(ExprLit {
-                            lit: Lit::Str(lit_str),
-                            ..
-                        }) = value
-                        {
-                            let mut raw_token = lit_str.token().to_string();
-                            if let Some(stripped) = raw_token.strip_prefix('\"') {
-                                raw_token = stripped.to_string();
-                            }
-                            if let Some(stripped) = raw_token.strip_suffix('\"') {
-                                raw_token = stripped.to_string();
-                            }
-                            // Collect every line of doc-comment.
-                            doc_comments.push(raw_token.trim().to_string());
-                        }
-                    }
-                });
-            }
-        });
+		// Every individual doc comment is an attr.
+		field.attrs.iter().for_each(|attr| {
+			if let Meta::NameValue(MetaNameValue { path, value, .. }) = &attr.meta {
+				path.segments.iter().for_each(|segment| {
+					if segment.ident == "doc" {
+						if let Expr::Lit(ExprLit {
+							lit: Lit::Str(lit_str),
+							..
+						}) = value
+						{
+							let mut raw_token = lit_str.token().to_string();
+							if let Some(stripped) = raw_token.strip_prefix('\"') {
+								raw_token = stripped.to_string();
+							}
+							if let Some(stripped) = raw_token.strip_suffix('\"') {
+								raw_token = stripped.to_string();
+							}
+							// Collect every line of doc-comment.
+							doc_comments.push(raw_token.trim().to_string());
+						}
+					}
+				});
+			}
+		});
 
-        if doc_comments.is_empty() {
-            return "No doc comment found".to_string();
-        }
-        doc_comments.join(" ")
-    })
+		if doc_comments.is_empty() {
+			return "No doc comment found".to_string();
+		}
+		doc_comments.join(" ")
+	})
 }
 
 /// Parse fields for the widgets to generate from the `#[control]` field attributes.
 fn parse_widgets_from_fields(fields: &Fields) -> impl Iterator<Item = TokenStream2> + '_ {
-    fields
-    .iter()
-    .flat_map(|field| {
-        let name = field.ident.clone().unwrap();
-        field
-        .attrs
-        .iter()
-        .filter_map(move |attr| {
-            if let Meta::List(MetaList { path, tokens, .. }) = &attr.meta {
-                if path.into_token_stream().to_string() == "control" {
-                    let mut token_iter = tokens.clone().into_iter();
-                    if let Some(proc_macro2::TokenTree::Ident(ident)) = token_iter.next() {
-                        if ident == "slider" {
-                            let proc_macro2::TokenTree::Group(group) =
-                                token_iter.next().expect("slider to be provided a InclusiveRange prop") else {
-                                panic!("slider expects an InclusiveRange prop.");
-                            };
-                            let stream = group.stream();
-                            return Some(quote!(
-                                ::eframe::egui::Slider::new(&mut self.#name, #stream)
-                            ))
-                        }
-                        else if ident == "textbox" {
-                            return Some(quote!(
-                                ::eframe::egui::TextEdit::singleline(&mut self.#name).hint_text("")
-                            ))
-                        }
-                        return None;
-                    }
-                }
-            }
-            None
-        })
-    })
+	fields.iter().flat_map(|field| {
+		let name = field.ident.clone().unwrap();
+		field.attrs.iter().filter_map(move |attr| {
+			if let Meta::List(MetaList { path, tokens, .. }) = &attr.meta {
+				if path.into_token_stream().to_string() == "control" {
+					let mut token_iter = tokens.clone().into_iter();
+					if let Some(proc_macro2::TokenTree::Ident(ident)) = token_iter.next() {
+						if ident == "slider" {
+							let proc_macro2::TokenTree::Group(group) = token_iter
+								.next()
+								.expect("slider to be provided a InclusiveRange prop")
+							else {
+								panic!("slider expects an InclusiveRange prop.");
+							};
+							let stream = group.stream();
+							return Some(quote!(
+									::bevy_egui::egui::Slider::new(&mut self.#name, #stream)
+							));
+						} else if ident == "textbox" {
+							return Some(quote!(
+									::bevy_egui::egui::TextEdit::singleline(&mut self.#name).hint_text("")
+							));
+						} else if ident == "bool" {
+							return Some(quote!{
+									::bevy_egui::egui::Checkbox::without_text(&mut self.#name)
+							})
+						}
+						return None;
+					}
+				}
+			}
+			None
+		})
+	})
 }
 
-/// Expand the parsed struct into a [eframe::egui::Grid] of three columns
+/// Expand the parsed struct into a [bevy_egui::egui::Grid] of three columns
 /// where the first column is the struct field name, the second column
 /// is the interactive form control, and the third field is the description
 /// of the field extracted from the doc comment.
 pub fn expand(input: DeriveInput) -> TokenStream {
-    let Data::Struct(DataStruct { fields, .. }) = &input.data else {
-        panic!("expected a struct");
-    };
+	let Data::Struct(DataStruct { fields, .. }) = &input.data else {
+		panic!("expected a struct");
+	};
 
-    let struct_name = &input.ident;
-    let grid_id = format!("{}__control-panel", input.ident);
+	let struct_name = &input.ident;
+	let grid_id = format!("{}__control-panel", input.ident);
 
-    let field_names = fields.iter().map(|field| &field.ident);
-    let field_docs = parse_doc_comments_from_fields(fields);
-    let field_widgets = parse_widgets_from_fields(fields);
+	let field_names = fields.iter().map(|field| &field.ident);
+	let field_docs = parse_doc_comments_from_fields(fields);
+	let field_widgets = parse_widgets_from_fields(fields);
 
-    let setting_heading = "Setting";
-    let value_heading = "Value";
-    let description_heading = "Description";
+	let setting_heading = "Setting";
+	let value_heading = "Value";
+	let description_heading = "Description";
 
-    let expanded = quote! {
+	let expanded = quote! {
 
-        impl #struct_name {
-            pub fn ui(&mut self, ui: &mut ::eframe::egui::Ui) -> ::eframe::egui::Response {
+			impl #struct_name {
+					pub fn ui(&mut self, ui: &mut ::bevy_egui::egui::Ui) -> ::bevy_egui::egui::Response {
 
-                ::eframe::egui::Grid::new(#grid_id)
-                .num_columns(3)
-                .spacing([40., 20.])
-                .striped(true)
-                .show(ui, |ui| {
-                    ui.heading(#setting_heading);
-                    ui.heading(#value_heading);
-                    ui.heading(#description_heading);
-                    ui.end_row();
-                    #(
-                        {
-                            ui.label(stringify!(#field_names));
-                            ui.add(#field_widgets);
-                            ui.vertical(|ui| ui.label(#field_docs));
-                            ui.end_row();
-                        }
-                    )*
-                })
-                .response
-            }
-        }
-    };
-    expanded.into()
+							::bevy_egui::egui::Grid::new(#grid_id)
+							.num_columns(3)
+							.spacing([40., 20.])
+							.striped(true)
+							.show(ui, |ui| {
+									// ui.heading(#setting_heading);
+									ui.heading(#value_heading);
+									ui.heading(#description_heading);
+									ui.end_row();
+									#(
+											{
+													// ui.label(stringify!(#field_names));
+													ui.add(#field_widgets);
+													ui.vertical(|ui| ui.label(#field_docs));
+													ui.end_row();
+											}
+									)*
+							})
+							.response
+					}
+			}
+	};
+	expanded.into()
 }
