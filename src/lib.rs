6412699f9b657168acc065bd74ffe03270c607ef
ActuallyHappening (Ubuntu) <105958073+ActuallyHappening@users.noreply.github.com>
2023-07-29 11:13:09 +1000

    chore: fix + fmt

--- src/lib.rs
+++ src/lib.rs
@@ -62,5 +62,5 @@ use syn::{parse_macro_input, DeriveInput};
 /// ```
 #[proc_macro_derive(ControlPanel, attributes(control))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    parse::expand(parse_macro_input!(input as DeriveInput))
+	parse::expand(parse_macro_input!(input as DeriveInput))
 }
